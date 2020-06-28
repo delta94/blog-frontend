02a1507ecd03a92c4503fe8325903fb2fdb90777
Julio Bermudez <jbermudezfc90@gmail.com>
2020-06-28 13:42:20 -0400

    Updates

---
+++ precache-manifest.b4aab4364d821a8aabf4695675c7c8b0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d7b056d48d6f5fdeee880e2d6810e19b",
+    "url": "/blog-frontend/index.html"
+  },
+  {
+    "revision": "5272e4e89e5effaa2135",
+    "url": "/blog-frontend/static/css/main.74fa18a6.chunk.css"
+  },
+  {
+    "revision": "864d5d6087b3e24e38d5",
+    "url": "/blog-frontend/static/js/2.5a830fb5.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/blog-frontend/static/js/2.5a830fb5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5272e4e89e5effaa2135",
+    "url": "/blog-frontend/static/js/main.eb18255f.chunk.js"
+  },
+  {
+    "revision": "6bcca72b0ad5b1d22ee0",
+    "url": "/blog-frontend/static/js/runtime-main.9e9a9755.js"
+  }
+]);
